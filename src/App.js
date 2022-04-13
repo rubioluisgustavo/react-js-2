c7b411f7f59dc9c8d6f26858550dbeb686cd6bb8
Luis Rubio <rubioluisgustavo@outlook.com>
2022-04-13 15:55:45 -0300

    correção

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Comentario from './components/Comentario';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Meu projeto em React JS</h1>
+      <Comentario />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Comentario.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Comentario.css';
+
+const Comentario = () => (
+    <div className="Comentario">
+        <div>João:</div>
+        <div>Olá, eu sou um component.</div>
+    </div>
+);
+
+export default Comentario;
